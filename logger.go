fb7df6f36730235df4f03d8ff2e9a622f1114e1f
Igor Kolomiyets <ikolomiyets@gmail.com>
2023-09-24 15:33:02 +0100

    Adding a response status into output

--- logger.go
+++ logger.go
@@ -1,34 +1,34 @@
 package logger
 
 import (
-    "github.com/gofiber/fiber/v2"
-    "log/slog"
-    "time"
+	"github.com/gofiber/fiber/v2"
+	"log/slog"
+	"time"
 )
 
 // New creates a new middleware handler
 func New(component string) fiber.Handler {
-    // Set variables
-    var (
-        Start time.Time
-        Stop  time.Time
-    )
+	// Set variables
+	var (
+		Start time.Time
+		Stop  time.Time
+	)
 
-    // Return new handler
-    return func(c *fiber.Ctx) error {
-        Start = time.Now()
+	// Return new handler
+	return func(c *fiber.Ctx) error {
+		Start = time.Now()
 
-        if component == "" {
-            component = "logger"
-        }
+		if component == "" {
+			component = "logger"
+		}
 
-        // Handle request, store err to return
-        chainErr := c.Next()
+		// Handle request, store err to return
+		chainErr := c.Next()
 
-        Stop = time.Now()
-        slog.Info("request", "component", component, "method", c.Method(), "response_time", Stop.Sub(Start).Round(time.Millisecond),
-            "source_ip", c.IP(), "method", c.Method(), "path", c.Path())
-        // End chain
-        return chainErr
-    }
+		Stop = time.Now()
+		slog.Info("request", "component", component, "method", c.Method(), "response_time", Stop.Sub(Start).Round(time.Millisecond),
+			"source_ip", c.IP(), "status", c.Response().StatusCode(), "path", c.Path())
+		// End chain
+		return chainErr
+	}
 }
