7c27c0bae65a6535929d809e6368f71b99214f05
Igor Kolomiyets <ikolomiyets@gmail.com>
2023-10-04 18:42:41 +0100

    Adding ability not to log certain routes

--- logger.go
+++ logger.go
@@ -8,11 +8,22 @@ import (
 
 // New creates a new middleware handler
 func New(component string) fiber.Handler {
+	return NewWithExclusions(component, nil)
+}
+
+// NewWithExclusions creates a new middleware handler with list of routes excluded from logging
+func NewWithExclusions(component string, exclusions []string) fiber.Handler {
 	// Set variables
 	var (
-		Start time.Time
-		Stop  time.Time
+		Start  time.Time
+		Stop   time.Time
+		routes map[string]interface{}
 	)
+
+	routes = make(map[string]interface{})
+	for _, exclusion := range exclusions {
+		routes[exclusion] = true
+	}
 
 	// Return new handler
 	return func(c *fiber.Ctx) error {
@@ -25,9 +36,19 @@ func New(component string) fiber.Handler {
 		// Handle request, store err to return
 		chainErr := c.Next()
 
-		Stop = time.Now()
-		slog.Info("request", "component", component, "method", c.Method(), "response_time", Stop.Sub(Start).Round(time.Millisecond),
-			"source_ip", c.IP(), "status", c.Response().StatusCode(), "path", c.Path())
+		log := true
+		if c.Route() != nil {
+			if routes[c.Route().Path] != nil {
+				log = false
+			}
+		}
+
+		if log {
+			Stop = time.Now()
+			slog.Info("request", "component", component, "method", c.Method(), "response_time", Stop.Sub(Start).Round(time.Millisecond),
+				"source_ip", c.IP(), "status", c.Response().StatusCode(), "path", c.Path())
+		}
+
 		// End chain
 		return chainErr
 	}
